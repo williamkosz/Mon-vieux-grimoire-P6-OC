4540243df8188b2a717ff9ceaa91d2e7a9650dc1
William Koszelnik <william.koszelnik@gmail.com>
2024-05-06 13:24:26 +0200

    Modifications des routes

--- back-end/routes/book.js
+++ back-end/routes/book.js
@@ -6,11 +6,9 @@ const multer = require("../middleware/multer-config");
 const bookCtrl = require("../controllers/book");
 
 
-router.post('/', auth,multer, sharp, bookCtrl.createBook ); 
-router.put("/:id",auth,multer, sharp, bookCtrl.modifyBook);
+router.post('/', auth,multer, bookCtrl.createBook ); 
+router.put("/:id",auth,multer, bookCtrl.modifyBook);
 router.delete ("/:id",auth, bookCtrl.deleteBook); 
-router.post("/:id/rating", auth, bookCtrl.giveRating);
-router.get("/bestrating", bookCtrl.bestRating);
 router.get('/', bookCtrl.getAllBooks);
 router.get("/:id", bookCtrl.getOneBook);
 
